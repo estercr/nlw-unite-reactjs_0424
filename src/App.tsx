61f027c1722b44e73a8b6fabb4b647944071424d
Ester Cristina da Cunha e. Silva <ester.cristina.silva@accenture.com>
2024-04-03 14:56:02 -0300

    adjust_screen_size

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Header } from "./components/header";
 
 export function App() {
   return (
-    <div className="max-w-screen-[1216px] mx-auto py-5 flex-col gap-5">
+    <div className="max-w-screen-2xl mx-auto py-5 flex-col gap-5">
       <Header />
       <AttendeeList />
     </div>
